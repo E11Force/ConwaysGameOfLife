a1de0b6ecb56491a160a040230d6fddaecc07fd0
kshemyakin <k.shemyakin@compassplus.com>
2025-08-15 11:43:54 +0500

    Refined UIElements header
    Successfully refined UIElements header code to make it as self-sustainable as possible.

--- include/UIElements.h
+++ include/UIElements.h
@@ -17,6 +17,13 @@ SDL_Texture* GetRenderedText(SDL_Renderer* renderer, const char* caption, float 
 	return text_texture;
 }
 
+SDL_Texture* LoadBMPImage(const char* image_file) {
+	SDL_Surface* image_surface = SDL_LoadBMP(image_file);
+	SDL_Texture* image_texture = SDL_CreateTextureFromSurface(renderer, image_surface);
+	SDL_DestroySurface(image_surface);
+	return image_texture;
+}
+
 //////////////////////////////
 //		LABEL DEFINITIONS
 //////////////////////////////
@@ -35,15 +42,11 @@ void InitLabelText(Label& label, SDL_Renderer* renderer, const char* caption, fl
 }
 
 void InitLabelImage(Label& label, SDL_Renderer* renderer, const char* image_file, SDL_FRect dstrect) {
-	SDL_Surface* image_surface = SDL_LoadBMP(image_file);
-	SDL_Texture* image_texture = SDL_CreateTextureFromSurface(renderer, image_surface);
-	SDL_DestroySurface(image_surface);
-
-	label.content = image_texture;
+	label.content = LoadBMPImage(image_file);
 	label.dstrect.x = dstrect.x;
 	label.dstrect.y = dstrect.y;
-	label.dstrect.w = (dstrect.w == 0 ? image_texture->w : dstrect.w);
-	label.dstrect.h = (dstrect.h == 0 ? image_texture->h : dstrect.h);
+	label.dstrect.w = (dstrect.w < 0 ? label.content->w : dstrect.w);
+	label.dstrect.h = (dstrect.h < 0 ? label.content->h : dstrect.h);
 }
 
 void DestroyLabel(Label* label) {
@@ -71,18 +74,45 @@ struct Button {
 	bool pushed = false;
 };
 
-void InitButtonText(Button& button, SDL_Renderer* renderer, SDL_FRect dstrect, const char* caption, float text_size, button_evt event = nullptr, void* evt_param = nullptr, unsigned char border_width = 4, unsigned char padding = 10) {
+static void InitBorderAndRect(Button& button, SDL_FRect dstrect, unsigned char border_width) {
 	button.dstrect = dstrect;
 	button.border_rect = {  button.dstrect.x - border_width,
 							button.dstrect.y - border_width,
 							button.dstrect.w + border_width * 2,
 							button.dstrect.h + border_width * 2};
-	button.content_rect = { button.dstrect.x + padding,
-							button.dstrect.y + padding,
-							button.dstrect.w - padding * 2,
-							button.dstrect.h - padding * 2 }; // remake content rect to conform to content dimensions
+}
 
+static void SetConformingContentRect(Button& button, unsigned char padding) {
+	SDL_FRect conform_rect = { button.dstrect.x + padding,
+							   button.dstrect.y + padding,
+							   button.dstrect.w - padding * 2,
+							   button.dstrect.h - padding * 2 };
+	float width_scale = conform_rect.w / button.content.w;
+	float height_scale = conform_rect.h / button.content.h;
+	if(button.content.h * width_scale <= conform_rect.h)
+		button.content_rect = { conform_rect.x,
+								conform_rect.y - (conform_rect.h - button.content.h * width_scale) / 2,
+								button.content.w * width_scale,
+								button.content.h * width_scale };
+	else
+		button.content_rect = { conform_rect.x - (conform_rect.w - button.content.w * height_scale) / 2,
+								conform_rect.y,
+								button.content.w * height_scale,
+								button.content.h * height_scale};
+}
+
+void InitButtonText(Button& button, SDL_Renderer* renderer, SDL_FRect dstrect, const char* caption, float text_size, button_evt event = nullptr, void* evt_param = nullptr, unsigned char border_width = 4, unsigned char padding = 10) {
+	InitBorderAndRect(button, dstrect, border_width);
 	button.content = GetRenderedText(renderer, caption, text_size);
+	SetConformingContentRect(button, padding);
+	button.event = event;
+	button.event_param = evt_param;
+}
+
+void InitButtonImage(Button& button, SDL_Renderer* renderer, SDL_FRect dstrect, const char* image_file, button_evt event = nullptr, void* evt_param = nullptr, unsigned char border_width = 4, unsigned char padding = 10) {
+	InitBorderAndRect(button, dstrect, border_width);
+	button.content = LoadBMPImage(image_file);
+	SetConformingContentRect(button, padding);
 	button.event = event;
 	button.event_param = evt_param;
 }
@@ -119,25 +149,22 @@ SDL_FRect GetButtonHoveredRect(const SDL_FRect& rect) {
 	return hovered;
 }
 
-void RenderButton(SDL_Renderer* renderer, const Button& button) {
-	static SDL_FPoint mouse_pos;
-	SDL_MouseButtonFlags mouse_buttons = SDL_GetMouseState(&mouse_pos.x, &mouse_pos.y);
-
-	if (!SDL_PointInRectFloat(&mouse_pos, &(button.pos)) || button.pushed) {
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-		SDL_RenderFillRect(renderer, &(button.pos));
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-		SDL_RenderFillRect(renderer, &(button.inner));
+void RenderButton(SDL_Renderer* renderer, const Button& button, SDL_FPoint& mouse_pos) {
+	static SDL_FRect border_rect;
+	static SDL_FRect dstrect;
+	
+	if (!SDL_PointInRectFloat(&mouse_pos, &(button.dstrect)) || button.pushed) {
+		border_rect = button.border_rect;
+		dstrect = button.dstrect;
 	}
 	else {
-		SDL_FRect border = GetButtonHoveredRect(button.pos);
-		SDL_FRect inner = GetButtonHoveredRect(button.inner);
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-		SDL_RenderFillRect(renderer, &border);
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-		SDL_RenderFillRect(renderer, &inner);
+		border_rect = GetButtonHoveredRect(button.border_rect);
+		dstrect = GetButtonHoveredRect(button.dstrect);
 	}
-
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-	SDL_RenderTextureTiled(renderer, button.caption, nullptr, button.caption_scale, &button.caption_box);
+	SDL_RenderFillRect(renderer, &border_rect);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderFillRect(renderer, &dstrect);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+	SDL_RenderTexture(renderer, button.content, nullptr, &button.content_rect);
 }
